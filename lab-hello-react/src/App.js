fa3708897ef7b8310f9bdc049bd7d28a4522e364
markixa <laiamr77@gmail.com>
2022-06-14 21:31:54 +0200

    logic added, styles pending

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+
+//import images
+import logo from './logo.svg';
+import icon1 from './images/icon1.png';
+import icon2 from './images/icon2.png';
+import icon3 from './images/icon3.png';
+import icon4 from './images/icon4.png';
+import ironhackLogo from './images/ironhack-logo-xs.png';
+import menuImg from './images/menu-top-xs.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="mainContainer">
+      <div class="firstContainer">
+        <img src="{ironhackLogo}" alt="Ironhack logo"/>
+        <img src="{menuImg}" alt="side menu logo"/>
+      </div>
+        <h1>Say hello to ReactJS</h1>
+        <h2>You will learn to use the most popular frontend library, and become a super Ninka developer.</h2>
+        <p>Awesome!</p>
+      <div class="secondContainer">
+        <div className="element">
+          <img src="{icon1}" alt="icon1 logo"/>
+          <h3>Declarative</h3>
+          <h4>React makes it painless to create interactiveUIs</h4>
+        </div>
+        <div className="element">
+          <img src="{icon2}" alt="icon2 logo"/>
+          <h3>Components</h3>
+          <h4>Build encapsulated components that manage their state</h4>
+        </div>
+        <div className="element">
+          <img src="{icon3}" alt="icon3 logo"/>
+          <h3>Single-way</h3>
+          <h4>A set of immutable values are passed to the components</h4>
+        </div>
+        <div className="element">
+          <img src="{icon4}" alt="icon4 logo"/>
+          <h3>JSX</h3>
+          <h4>Statically-typed, designed to run on modern browsers</h4>
+        </div>
+      </div>
+
     </div>
   );
 }
-
 export default App;
